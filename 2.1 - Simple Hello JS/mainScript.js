3d8030eccc55dc48932ee20e7647da58c971b5f6
Vladislav Azarevich <azarevichvl@mail.ru>
2019-06-14 16:20:47 +0300

    exercise 2.15

--- 2.1 - Simple Hello JS/mainScript.js
+++ 2.1 - Simple Hello JS/mainScript.js
@@ -1,3 +1,9 @@
-let name = "Василий";
-let admin = name;
-alert(admin);
+let number = prompt("Enter a number over 100:");
+while (number <= 100 && number != null){
+    number = prompt("Wrong number. Try again. Enter a number over 100:");
+}
+if (number != null){
+    alert("Eeeeeey! You entered " + number +"!");
+} else {
+    alert("You canceled action.");
+}
